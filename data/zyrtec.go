5a968036bb552dbf586b17835852ac9d5e585941
Dan Esparza <esparza.dan@gmail.com>
2019-04-16 15:34:40 -0400

    Complete zyrtec service

--- data/zyrtec.go
+++ data/zyrtec.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 // ZyrtecService is a pollen service for Zyrtec formatted data
@@ -13,6 +14,15 @@ type ZyrtecService struct{}
 // ZyrtecResponse is the native service return format
 type ZyrtecResponse struct {
 	PredominantPollen string `json:"predominantPollen"`
+	Location          string `json:"location"`
+	Zipcode           string `json:"zipcode"`
+	Date              string `json:"date"`
+	AllergyScore      string `json:"allergyScore"`
+	Forecast          struct {
+		Extended []struct {
+			PollenScore float64 `json:"pollenScore,omitempty"`
+		} `json:"extended"`
+	} `json:"forecast"`
 }
 
 // GetPollenReport gets the pollen report
@@ -56,7 +66,30 @@ func (s ZyrtecService) GetPollenReport(zipcode string) (PollenReport, error) {
 		return retval, apperr
 	}
 
-	log.Printf("Response: %+v", serviceResponse)
+	//	Parse the date/time:
+	layoutBaseZyrtec := "2006-01-02"
+	parsedStartDate, err := time.Parse(layoutBaseZyrtec, serviceResponse.Date)
+	if err != nil {
+		apperr := fmt.Errorf("There was a problem parsing the date in the response from Zyrtec API: %s", err)
+		return retval, apperr
+	}
+
+	//	Parse the data items:
+	dataitems := []float64{}
+	for _, item := range serviceResponse.Forecast.Extended {
+		if item.PollenScore != 0 {
+			dataitems = append(dataitems, item.PollenScore)
+		}
+	}
+
+	//	Set the properties in the return object:
+	retval = PollenReport{
+		PredominantPollen: serviceResponse.PredominantPollen,
+		Zipcode:           serviceResponse.Zipcode,
+		City:              serviceResponse.Location,
+		StartDate:         parsedStartDate,
+		Data:              dataitems,
+	}
 
 	return retval, nil
 }
--- data/zyrtec_test.go
+++ data/zyrtec_test.go
@@ -12,11 +12,13 @@ func TestZyrtec_GetPollenReport_ReturnsValidData(t *testing.T) {
 	zipcode := "30019"
 
 	//	Act
-	_, err := service.GetPollenReport(zipcode)
+	response, err := service.GetPollenReport(zipcode)
 
 	//	Assert
 	if err != nil {
 		t.Errorf("Error calling GetPollenReport: %v", err)
 	}
+
+	t.Logf("Returned object: %+v", response)
 
 }
